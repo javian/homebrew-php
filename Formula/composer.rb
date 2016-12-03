2ca53a4bdb0b7674077cdaaaa09f82a3393195ec
Jan Viljanen <jan.viljanen@greenpeace.org>
2016-12-03 09:27:35 +0100

    bump revision

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -6,6 +6,7 @@ class Composer < AbstractPhpPhar
   homepage "http://getcomposer.org"
   url "https://getcomposer.org/download/1.2.2/composer.phar"
   sha256 "c6ae4e099351233f5f82ade9738475eec6587aadf9ffe66fec4c350b329fbb32"
+  revision 1
   head "https://getcomposer.org/composer.phar"
 
   bottle do
